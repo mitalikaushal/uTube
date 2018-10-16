cf381efb1af49e6212f3c1e5e1bdb8ab25ed7c94
Mitali Kaushal <mitali.kaushal9@gmail.com>
2018-10-16 03:03:59 -0700

    wrapping the project

--- src/index.js
+++ src/index.js
@@ -15,16 +15,20 @@ class App  extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { videos: [], selectedVideo: null };
+        this.state = { videos: [], selectedVideo: null };   
 
-        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
-          this.setState({ videos:videos, selectedVideo: videos[0] });
+        this.videoSearch('music');
+    }
+
+    videoSearch(term) {
+        YTSearch({ key: API_KEY, term: term }, videos => {
+        this.setState({ videos: videos, selectedVideo: videos[0] });
         });
     }
     render() {
         return (
             <div>
-                <SearchBar />
+                <SearchBar onSearchTermChange = {term => this.videoSearch(term)} />
                 <VideoDescription video ={this.state.selectedVideo} />
                 <Titles onVideoSelect= {selectedVideo => this.setState({selectedVideo})} videos={this.state.videos} />
             </div>
